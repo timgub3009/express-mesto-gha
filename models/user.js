2ea1ed7ade38c07e93e4ba48cb726d280dd4f649
Gubeidulin Timur <timur3009@yandex.ru>
2023-04-29 14:38:28 +0300

    password issue fixed?

--- models/user.js
+++ models/user.js
@@ -3,38 +3,45 @@ const { isEmail } = require('validator');
 const mongoose = require('mongoose');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    default: 'Жак-Ив Кусто',
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
-  },
-  about: {
-    type: String,
-    default: 'Исследователь',
-    minlength: [2, 'Минимальная длина поля "about" - 2'],
-    maxlength: [30, 'Максимальная длина поля "about" - 30'],
-  },
-  avatar: {
-    type: String,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-  },
-  email: {
-    type: String,
-    required: [true, 'Поле "email" должно быть заполнено'],
-    unique: true,
-    validate: {
-      validator: (email) => isEmail(email),
-      message: 'Некорректный email',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      default: 'Жак-Ив Кусто',
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+    about: {
+      type: String,
+      default: 'Исследователь',
+      minlength: [2, 'Минимальная длина поля "about" - 2'],
+      maxlength: [30, 'Максимальная длина поля "about" - 30'],
+    },
+    avatar: {
+      type: String,
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    },
+    email: {
+      type: String,
+      required: [true, 'Поле "email" должно быть заполнено'],
+      unique: true,
+      validate: {
+        validator: (email) => isEmail(email),
+        message: 'Некорректный email',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Поле "password" должно быть заполнено'],
+      select: false,
     },
   },
-  password: {
-    type: String,
-    required: [true, 'Поле "password" должно быть заполнено'],
-    select: false,
+  {
+    toObject: {
+      useProjection: true,
+    },
   },
-});
+);
 
 // eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
