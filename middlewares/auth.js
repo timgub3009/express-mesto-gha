4554b15c2a88f7aed9e9ca70e0659b1563dbf89e
Gubeidulin Timur <timur3009@yandex.ru>
2023-04-29 16:26:42 +0300

    cookie problem fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,8 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  const token = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
-  }
-
-  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
