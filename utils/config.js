99c7710e18c9a3326df361ae838509bf67bff8c0
Gubeidulin Timur <timur3009@yandex.ru>
2023-04-30 14:44:43 +0300

    regex fixed

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,7 @@ const FORBIDDEN_ERROR_CODE = 403;
 const NOT_FOUND_ERROR_CODE = 404;
 const CONFLICT_ERROR_CODE = 409;
 
-const regex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
+const regex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_[\]+.~#?&[\]/=]*)$/;
 
 const { PORT = 3000 } = process.env;
 const DATABASE = 'mongodb://localhost:27017/mestodb';
